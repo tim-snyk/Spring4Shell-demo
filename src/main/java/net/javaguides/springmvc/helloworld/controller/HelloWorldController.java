029ec039f3ddae8ff5a71bdb9b8c942a163b7990
TIM GOWAN <94128658+tim-gowan@users.noreply.github.com>
2022-04-03 16:15:43 -0500

    Modified to introduce Spring4Shell Vulnerability

---
+++ src/main/java/net/javaguides/springmvc/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,26 @@
+package net.javaguides.springmvc.helloworld.controller;
+
+import java.time.LocalDateTime;
+import net.javaguides.springmvc.helloworld.model.HelloWorld;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+
+/**
+ * @author Ramesh Fadatare
+ */
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/helloworld")
+	public void handler(HelloWorld model) {
+
+		//HelloWorld helloWorld = new HelloWorld();
+		//helloWorld.setMessage("Hello World Example Using Spring MVC 5!!!");
+		//helloWorld.setDateTime(LocalDateTime.now().toString());
+		//model.addAttribute("helloWorld", helloWorld);
+		//return "helloworld";
+	}
+}
